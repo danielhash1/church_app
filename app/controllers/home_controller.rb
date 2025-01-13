0326c6e876abd5502943895e66a7b23b108ce98f
Daniel Hash <hashdan542@gmail.com>
2025-01-13 20:43:42 +0100

    Fixed order posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
     @carousel_images = CarouselImage.order(:position)
   end
 end
